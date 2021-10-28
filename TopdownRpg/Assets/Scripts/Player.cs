46e1304ac65cb04e721aaa292a28bac2235de0a1
rafaelmaia23 <rafaelmaiadafonseca@gmail.com>
2021-10-28 12:02:32 -0300

    add more animations and running moviment
    Added running movemente with left shift.
    Added walking and running animations.

--- TopdownRpg/Assets/Scripts/Player.cs
+++ TopdownRpg/Assets/Scripts/Player.cs
@@ -4,22 +4,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+    private float initialSpeed;
+    private Rigidbody2D rig;   
+        
+    private Vector2 _direction;
+    public Vector2 Direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
 
-    private Rigidbody2D rig;
-    private Vector2 direction;
+    private bool _isRunning;
+    public bool IsRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
     private void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+
+        OnRun();
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+        OnMove();
     }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    #endregion
 }
---
+++ TopdownRpg/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Player player;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+    #region Movement
+    void OnMove()
+    {
+        if (player.Direction.sqrMagnitude > 0)
+        {
+            anim.SetInteger("Transition", 1);
+        }
+        else
+        {
+            anim.SetInteger("Transition", 0);
+        }
+
+        if (player.Direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (player.Direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    void OnRun()
+    {
+        if (player.IsRunning)
+        {
+            anim.SetInteger("Transition", 2);
+        }
+    }
+
+    #endregion
+}
